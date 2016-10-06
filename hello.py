cbade2b9843c3b8eacf38e84b088d225fb5dae1b
dudgkr114 <dudgkr6751@naver.com>
2016-10-06 15:33:20 +0900

    hello.py modified on github

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
+//for command line git tutorial
+//github modification
+
 print( "Hollo World" )
 print( "Tell your World" )
 print( "Tell his World" )
 print( "Tell her World" )
+print( "겨ㅑㅇ그니병시나")
